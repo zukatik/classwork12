e6582b1202b4089f9aed4e471484ad9d7bfbdd4f
Таня <t.tikhonova0912@gmail.com>
2024-11-26 19:16:31 +0300

    Initial version of example.

---
+++ array.py
@@ -0,0 +1,14 @@
+arr = list()
+n = int(input("Enter number of elements: "))
+print("Enter elements:")
+i = 0
+while (i < n):
+tmp = int(input(""))
+arr.append(tmp)
+i += 1
+print("Array")
+i = 0
+while (i &lt; n):
+print(arr[i], end =" ")
+i += 1
+print("")
